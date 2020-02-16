145401e4d90a248924f90c96ddb7f323f989401d
emmanuel.nuyttens <emmanuel.nuyttens@outlook.com>
2020-02-16 12:35:34 +0100

    Initial commit

---
+++ src/Api/Controllers/StudentController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+using Logic.AppServices;
+using Logic.Dtos;
+using Logic.Students;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/students")]
+    public sealed class StudentController : BaseController
+    {
+        private readonly MessageBus _messageBus;
+
+        public StudentController(MessageBus messages)
+        {
+            _messageBus = messages;
+        }
+
+        #region Queries
+
+        [HttpGet]
+        public IActionResult GetList(string enrolled, int? number)
+        {
+            List<StudentDto> list = _messageBus.Dispatch(new GetListQuery(enrolled, number));
+            return Ok(list);
+        }
+
+        #endregion Queries
+
+
+        #region Commands
+
+        [HttpPost]
+        public IActionResult Register([FromBody] NewStudentDto dto)
+        {
+            var command = new RegisterCommand(
+                dto.Name, dto.Email,
+                dto.Course1, dto.Course1Grade,
+                dto.Course2, dto.Course2Grade);
+
+            Result result = _messageBus.Dispatch(command);
+            return FromResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Unregister(long id)
+        {
+            Result result = _messageBus.Dispatch(new UnregisterCommand(id));
+            return FromResult(result);
+        }
+
+        [HttpPost("{id}/enrollments")]
+        public IActionResult Enroll(long id, [FromBody] StudentEnrollmentDto dto)
+        {
+            Result result = _messageBus.Dispatch(new EnrollCommand(id, dto.Course, dto.Grade));
+            return FromResult(result);
+        }
+
+        [HttpPut("{id}/enrollments/{enrollmentNumber}")]
+        public IActionResult Transfer(
+            long id, int enrollmentNumber, [FromBody] StudentTransferDto dto)
+        {
+            Result result = _messageBus.Dispatch(new TransferCommand(id, enrollmentNumber, dto.Course, dto.Grade));
+            return FromResult(result);
+        }
+
+        [HttpPost("{id}/enrollments/{enrollmentNumber}/deletion")]
+        public IActionResult Disenroll(
+            long id, int enrollmentNumber, [FromBody] StudentDisenrollmentDto dto)
+        {
+            Result result = _messageBus.Dispatch(new DisenrollCommand(id, enrollmentNumber, dto.Comment));
+            return FromResult(result);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditPersonalInfo(long id, [FromBody] StudentPersonalInfoDto dto)
+        {
+            var command = new EditPersonalInfoCommand(id, dto.Name, dto.Email);
+            Result result = _messageBus.Dispatch(command);
+
+            return FromResult(result);
+        }
+
+        #endregion Commands
+    }
+}
